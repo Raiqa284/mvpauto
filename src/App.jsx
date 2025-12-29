ffae9d60bff8fb141e9311808747e0e8ed6aa172
Raiqa Brown <raiqa.brown@icloud.com>
2025-12-29 16:51:56 +0200

    added Title and about us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Background from './components/Background/Background'
 import Programs_temp from './components/Programs_temp/Programs_temp'
+import Title from './components/Title/Title'
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar/>
       <Background/>
       <div className="container">
+        <Title subTitle='Our Program' title='What We Offer'/>
           <Programs_temp/>
       </div>
       
---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about_img.png'//must still add image 
+import play-icon from '../../assets/play-icon.png'//must still add image 
+
+const About = () => {
+  return (
+    <div className= 'about'>
+       <div className="about-left"></div>
+       <img src="" alt="" />
+         <div className="about-right"></div>
+    </div>
+  )
+}
+
+export default About
